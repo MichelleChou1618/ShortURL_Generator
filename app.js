231d01457a21cfd75547236772a03ab44b52bc86
MichelleChou1618 <starresttmichelle@gmail.com>
2023-08-17 16:49:54 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -4,6 +4,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// 載入 shortURL model
+const ShortURL = require('./models/shortURL')
+
+// 載入 mongoose
+const mongoose = require('mongoose') 
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// 設定連線到 mongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) 
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // setting body-parser
 //不管從哪個路由發送過來的請求，都先經過 bodyParser 進行前置處理
